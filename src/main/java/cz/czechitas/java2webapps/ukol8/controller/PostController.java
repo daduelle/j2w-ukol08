a9f63beb98bba3964873c1237177319bb46cdbdb
daduelle <dani.rysava@gmail.com>
2021-05-30 22:16:59 +0200

    @deglen: vymazaní nepoužívaného kódů a importů

--- src/main/java/cz/czechitas/java2webapps/ukol8/controller/PostController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol8/controller/PostController.java
@@ -1,21 +1,12 @@
 package cz.czechitas.java2webapps.ukol8.controller;
 
-import cz.czechitas.java2webapps.ukol8.entity.Post;
-import cz.czechitas.java2webapps.ukol8.repository.PostRepository;
+
 import cz.czechitas.java2webapps.ukol8.service.PostService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.web.util.UrlPathHelper;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import java.util.Optional;
 
 @Controller
 public class PostController {
--- src/main/java/cz/czechitas/java2webapps/ukol8/repository/PostRepository.java
+++ src/main/java/cz/czechitas/java2webapps/ukol8/repository/PostRepository.java
@@ -1,14 +1,11 @@
 package cz.czechitas.java2webapps.ukol8.repository;
 
-import com.sun.xml.bind.v2.model.core.ID;
 import cz.czechitas.java2webapps.ukol8.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
@@ -19,9 +16,5 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 //    vyber posty, které obsahují datum publikace a nejsou v budoucnosti
     @Query("SELECT p FROM Post p WHERE p.published <= NOW()")
     Page<Post> findALLPublished(Pageable pageable);
-
-//    vyber podle ID
-    Page<Post> findById(Long id, Pageable pageable);
-
 
 }
